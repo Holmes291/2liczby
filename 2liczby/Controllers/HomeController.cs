06616f869585ff03592f458402ad4abbcafdea3d
Czro <cezi444@gmail.com>
2017-05-26 08:56:39 +0200

    Start
    Start

---
+++ 2liczby/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2liczby.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+   
+        [HttpPost]
+        public ViewResult Index(Models.MLicz dane)
+        {
+            bool x1, x2;
+            double c1, c2;
+            x1 = double.TryParse(dane.liczba1, out c1);
+            x2 = double.TryParse(dane.liczba2, out c2);
+
+            if (x1 == true && x2 == true)
+            {
+                c1 = Convert.ToDouble(dane.liczba1);
+                c2 = Convert.ToDouble(dane.liczba2);
+                dane.dziaba = 0;
+                if (dane.coMaByc == "suma") dane.wynik = c1 + c2;
+                if (dane.coMaByc == "roznica") dane.wynik = c1 - c2;
+                if (dane.coMaByc == "iloczyn") dane.wynik = c1 * c2;
+                if (dane.coMaByc == "iloraz")
+                {
+                    if (c2 == 0) dane.dziaba = 2;
+                    else dane.wynik = c1 / c2;
+                }
+            }
+            else dane.dziaba = 1;
+            
+            return View(dane);
+        }
+        
+    }
+}
---
+++ 2liczby/Models/MLicz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _2liczby.Models
+{
+    public class MLicz
+    {
+        public string liczba1 { get; set; }
+        public string liczba2 { get; set; }
+        public double wynik { get; set; }
+        public string coMaByc { get; set; }
+        public int dziaba { get; set; }
+    }
+}
